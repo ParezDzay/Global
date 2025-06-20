11ee12d0c5318cdf5ee62e5da561d682e98c766b
ParezDzay <parez17@gmail.com>
2025-06-20 11:15:04 +0300

    Update global.py

--- global.py
+++ global.py
@@ -106,7 +106,7 @@ def safe_rerun():
 SURGERY_TYPES = [
     "Phaco", "PPV", "Pterygium", "Blepharoplasty",
     "Glaucoma OP", "KPL", "Trauma OP", "Enucleation",
-    "Injection", "Squint OP", "Other",
+    "Injection", "Squint OP", "DCR", "SOR", "Other",
 ]
 ROOMS = ["Room 1", "Room 2"]
 
